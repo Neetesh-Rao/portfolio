bd15171d9e48886360c0a192a0fd74b752911c41
Neetesh Kumar Yadav <neeteshyadav0206@email.com>
2026-03-04 19:08:45 +0530

    edit

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
+const Blog = require("./models/blog");
+const slugify = require("slugify");
+const mongoose = require("mongoose");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+//midlleware for token verification
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(403).json({ message: "Access denied" });
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
 
 
-app.get('/',(req,res)=>{
-  res.send('hello')
-})
 
 // Route for sending email
 app.post("/send-email", async (req, res) => {
   const { name, email, message } = req.body;
-  
-
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -47,7 +59,71 @@ app.post("/send-email", async (req, res) => {
     res.status(500).json({ success: false, message: "Email failed to send" });
   }
 });
+// Route for sending email
+app.post("/admin/login", (req, res) => {
+  const { email, password } = req.body;
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+  if (
+    email !== process.env.ADMIN_EMAIL ||
+    password !== process.env.ADMIN_PASS
+  ) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.json({ token });
 });
+//create blog
+app.post("/admin/blog", verifyToken, async (req, res) => {
+  const { title, content, excerpt } = req.body;
+
+  const blog = new Blog({
+    title,
+    slug: slugify(title, { lower: true }),
+    content,
+    excerpt,
+  });
+
+  await blog.save();
+  res.json(blog);
+});
+//get all blog
+app.get("/admin/blog", async (req, res) => {
+  const blogs = await Blog.find().sort({ createdAt: -1 });
+  res.json(blogs);
+});
+//get particular
+app.get("/admin/blog/:slug", async (req, res) => {
+  const blog = await Blog.findOne({ slug: req.params.slug });
+  res.json(blog);
+});
+//update blog
+app.put("/admin/blog/:id", verifyToken, async (req, res) => {
+  const updated = await Blog.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.json(updated);
+});
+//delete blog
+app.delete("/admin/blog/:id", verifyToken, async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted successfully" });
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(5000, () => {
+      console.log("Server running on http://localhost:5000");
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error:", err);
+  });
